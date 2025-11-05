3e0b20923b6236c02d136eeb3a39c9fffa7e94b3
Leroy B <shoked@gmail.com>
2025-11-04 21:18:55 -0500

    First comit

---
+++ app.js
@@ -0,0 +1,13 @@
+import "dotenv/config";
+import express from "express";
+import morgan from "morgan";
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(express.json());
+app.get("/", (req, res) => {
+  req.status(200).send("Hello World!");
+});
+
+export default app;
